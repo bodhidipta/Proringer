7bfdee2c8fd9ca0ec976aabe1bc1ede8b06c47a9
surajcse116 <suraj.shaw@esolzmail.com>
2017-07-20 13:56:22 +0530

    delete function done in myProject List ,resend confirmation ,20 jul

--- app/src/main/java/com/android/llc/proringer/activities/PostedFinishActivity.java
+++ app/src/main/java/com/android/llc/proringer/activities/PostedFinishActivity.java
@@ -5,17 +5,19 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
 import android.view.View;
 import android.widget.ImageView;
 
 import com.android.llc.proringer.R;
+import com.android.llc.proringer.viewsmod.textview.ProRegularTextView;
 
 /**
  * Created by su on 7/14/17.
  */
 
 public class PostedFinishActivity extends AppCompatActivity {
-
+    ProRegularTextView mail_resent,contact;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +32,28 @@ public class PostedFinishActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+
+        mail_resent= (ProRegularTextView) findViewById(R.id.mail_resent);
+        contact= (ProRegularTextView) findViewById(R.id.contact);
+
+
+        mail_resent.setText(Html.fromHtml(getString(R.string.posted_finish_resent)));
+        contact.setText(Html.fromHtml(getString(R.string.posted_finish_contact_us)));
+
+
+        mail_resent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        contact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(PostedFinishActivity.this,ContactUsActivity.class));
             }
         });
 
