dfa8bea79d68814dd0fc879b48fc709f11bb04e5
surajcse116 <suraj.shaw@esolzmail.com>
2017-07-20 15:29:07 +0530

    delete function done in myProject List ,resend confirmation ,project details api fire,20 jul

--- app/src/main/java/com/android/llc/proringer/fragments/main_content/MyProjectDetails.java
+++ app/src/main/java/com/android/llc/proringer/fragments/main_content/MyProjectDetails.java
@@ -1,23 +1,33 @@
 package com.android.llc.proringer.fragments.main_content;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-
 import com.android.llc.proringer.R;
 import com.android.llc.proringer.appconstant.ProApplication;
+import com.android.llc.proringer.helper.ProServiceApiHelper;
 import com.android.llc.proringer.viewsmod.textview.ProRegularTextView;
 import com.bumptech.glide.Glide;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by su on 7/17/17.
  */
 
 public class MyProjectDetails extends Fragment {
+    ProgressDialog dialog;
+    ProRegularTextView tv_posted_in,tv_project,tv_service,tv_type,tv_property,tv_status,tv_start,img_description;
+    ImageView img_project;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -27,28 +37,76 @@ public class MyProjectDetails extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        ProRegularTextView tv_posted_in = (ProRegularTextView) view.findViewById(R.id.tv_posted_in);
-        ProRegularTextView tv_project = (ProRegularTextView) view.findViewById(R.id.tv_project);
-        ProRegularTextView tv_service = (ProRegularTextView) view.findViewById(R.id.tv_service);
-        ProRegularTextView tv_type = (ProRegularTextView) view.findViewById(R.id.tv_type);
-        ProRegularTextView tv_property = (ProRegularTextView) view.findViewById(R.id.tv_property);
-        ProRegularTextView tv_status = (ProRegularTextView) view.findViewById(R.id.tv_status);
+
+         tv_posted_in = (ProRegularTextView) view.findViewById(R.id.tv_posted_in);
+         tv_project = (ProRegularTextView) view.findViewById(R.id.tv_project);
+         tv_service = (ProRegularTextView) view.findViewById(R.id.tv_service);
+         tv_type = (ProRegularTextView) view.findViewById(R.id.tv_type);
+         tv_property = (ProRegularTextView) view.findViewById(R.id.tv_property);
+         tv_status = (ProRegularTextView) view.findViewById(R.id.tv_status);
 //        ProRegularTextView tv_type2 = (ProRegularTextView) view.findViewById(R.id.tv_type2);
-        ProRegularTextView tv_start = (ProRegularTextView) view.findViewById(R.id.tv_start);
-        ProRegularTextView img_description = (ProRegularTextView) view.findViewById(R.id.img_description);
-        ImageView img_project = (ImageView) view.findViewById(R.id.img_project);
+         tv_start = (ProRegularTextView) view.findViewById(R.id.tv_start);
+         img_description = (ProRegularTextView) view.findViewById(R.id.img_description);
+         img_project = (ImageView) view.findViewById(R.id.img_project);
 
-        tv_posted_in.setText(ProApplication.getInstance().getDataSelected().getDate_time());
-        tv_project.setText(ProApplication.getInstance().getDataSelected().getProject_category_name());
-        tv_service.setText(ProApplication.getInstance().getDataSelected().getProject_category_service_name());
-        tv_type.setText(ProApplication.getInstance().getDataSelected().getProject_service_looktype());
-        tv_property.setText(ProApplication.getInstance().getDataSelected().getProperty_type_name());
-        tv_status.setText(ProApplication.getInstance().getDataSelected().getProject_stage());
-        tv_start.setText(ProApplication.getInstance().getDataSelected().getProject_timeframe_name());
-        img_description.setText(ProApplication.getInstance().getDataSelected().getProject_name());
+        ProServiceApiHelper.getInstance(getActivity()).getMyProjectDetails(new ProServiceApiHelper.getApiProcessCallback() {
+            @Override
+            public void onStart() {
+                dialog = new ProgressDialog(getActivity());
+                dialog.setTitle("My Projects");
+                dialog.setCancelable(false);
+                dialog.setMessage("Getting MyProject details. Please wait.");
+                dialog.show();
+            }
 
-        if (!ProApplication.getInstance().getDataSelected().getProject_image().equals(""))
-            Glide.with(getActivity()).load(ProApplication.getInstance().getDataSelected().getProject_image()).centerCrop().into(img_project);
+            @Override
+            public void onComplete(String message) {
+                if (dialog != null && dialog.isShowing())
+                    dialog.dismiss();
+
+                try {
+                    JSONObject jsonObject = new JSONObject(message);
+
+                    if (jsonObject.has("info_array")) {
+
+                        JSONArray info_array=jsonObject.getJSONArray("info_array");
+
+                        tv_posted_in.setText(ProApplication.getInstance().getDataSelected().getDate_time());
+                        tv_project.setText(ProApplication.getInstance().getDataSelected().getProject_category_name());
+                        tv_service.setText(ProApplication.getInstance().getDataSelected().getProject_category_service_name());
+                        tv_type.setText(ProApplication.getInstance().getDataSelected().getProject_service_looktype());
+                        tv_property.setText(ProApplication.getInstance().getDataSelected().getProperty_type_name());
+                        tv_status.setText(ProApplication.getInstance().getDataSelected().getProject_stage());
+                        tv_start.setText(ProApplication.getInstance().getDataSelected().getProject_timeframe_name());
+                        img_description.setText(ProApplication.getInstance().getDataSelected().getProject_name());
+
+                        if (!ProApplication.getInstance().getDataSelected().getProject_image().equals(""))
+                            Glide.with(getActivity()).load(info_array.getJSONObject(0).getString("project_image")).centerCrop().into(img_project);
+
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onError(String error) {
+                if (dialog != null && dialog.isShowing())
+                    dialog.dismiss();
+
+                new AlertDialog.Builder(getActivity())
+                        .setTitle("MyProjects Error")
+                        .setMessage("" + error)
+                        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.dismiss();
+                            }
+                        })
+                        .show();
+            }
+        }, ProApplication.getInstance().getDataSelected().getId());
 
     }
 }
--- app/src/main/java/com/android/llc/proringer/helper/ProServiceApiHelper.java
+++ app/src/main/java/com/android/llc/proringer/helper/ProServiceApiHelper.java
@@ -80,6 +80,7 @@ public class ProServiceApiHelper {
     private String faqInformationAPI = "http://esolz.co.in/lab6/proringer_latest/app_faq";
     private String contactUsAPI = "http://esolz.co.in/lab6/proringer_latest/app_contact_us";
     private String myProjectDeleteAPI = "http://esolz.co.in/lab6/proringer_latest/app_myproject_delete";
+    private String myProjectdetailsAPI = "http://esolz.co.in/lab6/proringer_latest/app_myproject_details?user_id=";
 
     public static ProServiceApiHelper getInstance(Context context) {
         if (instance == null)
@@ -1380,6 +1381,72 @@ public class ProServiceApiHelper {
     }
 
     /**
+     * get MyProject details
+     *
+     * @param callback
+     * @param params
+     */
+    public void getMyProjectDetails(final getApiProcessCallback callback, String... params) {
+        if (NetworkUtil.getInstance().isNetworkAvailable(mcontext)) {
+            new AsyncTask<String, Void, String>() {
+
+                String exception = "";
+
+                @Override
+                protected void onPreExecute() {
+                    super.onPreExecute();
+                }
+
+                @Override
+                protected String doInBackground(String... params) {
+                    try {
+                        OkHttpClient client = new OkHttpClient.Builder().connectTimeout(6000, TimeUnit.MILLISECONDS).retryOnConnectionFailure(true).build();
+
+                        String ApiOption = myProjectdetailsAPI + "" + ProApplication.getInstance().getUserId()+"&project_id="+params[0];
+                        Logger.printMessage("myProjectdetailsAPI", "" + ApiOption);
+
+                        Request request = new Request.Builder()
+                                .get()
+                                .url(ApiOption)
+                                .build();
+
+                        Response response = client.newCall(request).execute();
+                        String responseString = response.body().string();
+                        Logger.printMessage("home_svhe", "" + responseString);
+                        JSONObject jsonObject = new JSONObject(responseString);
+                        if (jsonObject.getBoolean("response")) {
+                            return responseString;
+                        } else {
+                            exception = jsonObject.getString("message");
+                            return exception;
+                        }
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        exception = e.getMessage();
+                    }
+                    return null;
+                }
+
+                @Override
+                protected void onPostExecute(String s) {
+                    super.onPostExecute(s);
+                    if (exception.equals("")) {
+                        callback.onComplete(s);
+                    } else {
+                        callback.onError(s);
+                    }
+                }
+            }.executeOnExecutor(AsyncTask.SERIAL_EXECUTOR, params);
+
+        } else {
+            callback.onError("No internet connection found. Please check your internet connection.");
+        }
+
+    }
+
+
+    /**
      * get MyProject List
      *
      * @param callback
@@ -1481,6 +1548,8 @@ public class ProServiceApiHelper {
         }
 
     }
+
+
 
     /**
      * Search area by google API
