3e2b528f3fcf26479707ba9334906dcd8f702c27
surajcse116 <suraj.shaw@esolzmail.com>
2017-07-20 16:31:33 +0530

    delete function done in myProject List ,resend confirmation ,project details api fire,favourite list api fire 20 jul

--- app/src/main/java/com/android/llc/proringer/adapter/SearchFavouriteListAdapter.java
+++ app/src/main/java/com/android/llc/proringer/adapter/SearchFavouriteListAdapter.java
@@ -7,9 +7,14 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 import com.android.llc.proringer.R;
 import com.android.llc.proringer.activities.ProjectDetailsActivity;
+import com.android.llc.proringer.appconstant.ProApplication;
+import com.android.llc.proringer.viewsmod.textview.ProRegularTextView;
+import com.android.llc.proringer.viewsmod.textview.ProSemiBoldTextView;
+import com.bumptech.glide.Glide;
 
 import org.json.JSONArray;
 
@@ -34,7 +39,38 @@ public class SearchFavouriteListAdapter extends RecyclerView.Adapter<SearchFavou
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
         holder.ratingBar.setMax(5);
-        holder.ratingBar.setRating(4);
+        try {
+            holder.ratingBar.setRating(Float.valueOf(jsonInfoArray.getJSONObject(position).getString("pro_avg_review_rating")));
+            holder.tv_pros_company_name.setText(jsonInfoArray.getJSONObject(position).getString("pros_company_name"));
+            holder.tv_category_name.setText(jsonInfoArray.getJSONObject(position).getString("category_name"));
+            holder.tv_address.setText(jsonInfoArray.getJSONObject(position).getString("city")
+            +","+jsonInfoArray.getJSONObject(position).getString("state"));
+
+            holder.tv_total_review.setText("("+jsonInfoArray.getJSONObject(position).getString("total_review")+")");
+
+            if (!jsonInfoArray.getJSONObject(position).getString("profile_img").equals(""))
+                Glide.with(mcontext).load(jsonInfoArray.getJSONObject(position).getString("profile_img")).centerCrop().into(holder.img_project);
+
+
+            if(jsonInfoArray.getJSONObject(position).getString("pros_verify").equalsIgnoreCase("Y")){
+                holder.img_verify.setVisibility(View.VISIBLE);
+                holder.img_verify_tick.setVisibility(View.VISIBLE);
+                holder.tv_verify.setVisibility(View.VISIBLE);
+            }else {
+                holder.img_verify.setVisibility(View.GONE);
+                holder.img_verify_tick.setVisibility(View.GONE);
+                holder.tv_verify.setVisibility(View.GONE);
+            }
+
+
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+
+
 
         holder.main_container.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,11 +90,26 @@ public class SearchFavouriteListAdapter extends RecyclerView.Adapter<SearchFavou
     class ViewHolder extends RecyclerView.ViewHolder {
         RatingBar ratingBar;
         CardView main_container;
+        ProSemiBoldTextView tv_pros_company_name;
+        ProRegularTextView tv_category_name,tv_address,tv_verify,tv_total_review;
+        ImageView img_project,img_verify,img_verify_tick;
 
         public ViewHolder(View itemView) {
             super(itemView);
             ratingBar = (RatingBar) itemView.findViewById(R.id.ratingPro);
+
             main_container= (CardView) itemView.findViewById(R.id.main_container);
+
+            tv_pros_company_name= (ProSemiBoldTextView) itemView.findViewById(R.id.tv_pros_company_name);
+
+            tv_category_name= (ProRegularTextView) itemView.findViewById(R.id.tv_category_name);
+            tv_total_review= (ProRegularTextView) itemView.findViewById(R.id.tv_total_review);
+            tv_address= (ProRegularTextView) itemView.findViewById(R.id.tv_address);
+            tv_verify= (ProRegularTextView) itemView.findViewById(R.id.tv_verify);
+
+            img_project= (ImageView) itemView.findViewById(R.id.img_project);
+            img_verify= (ImageView) itemView.findViewById(R.id.img_verify);
+            img_verify_tick= (ImageView) itemView.findViewById(R.id.img_verify_tick);
 
         }
     }
