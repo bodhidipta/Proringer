bd39048998a50a0b6512090f81b0785303eb2959
surajcse116 <suraj.shaw@esolzmail.com>
2017-07-21 11:46:34 +0530

    delete function done in myProject List ,resend confirmation ,project details api fire,favourite list api fire,remove favourite pros from favourite list 20 jul

--- app/src/main/java/com/android/llc/proringer/activities/PostedFinishActivity.java
+++ app/src/main/java/com/android/llc/proringer/activities/PostedFinishActivity.java
@@ -7,8 +7,15 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Html;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.text.style.ForegroundColorSpan;
 import android.view.View;
 import com.android.llc.proringer.R;
+import com.android.llc.proringer.utils.Logger;
 import com.android.llc.proringer.viewsmod.textview.ProRegularTextView;
 
 /**
@@ -34,35 +41,82 @@ public class PostedFinishActivity extends AppCompatActivity {
             }
         });
 
-        mail_resent= (ProRegularTextView) findViewById(R.id.mail_resent);
-        contact= (ProRegularTextView) findViewById(R.id.contact);
+        contact = (ProRegularTextView) findViewById(R.id.contact);
+        mail_resent = (ProRegularTextView) findViewById(R.id.mail_resent);
+        contact.setMovementMethod(LinkMovementMethod.getInstance());
+        mail_resent.setMovementMethod(LinkMovementMethod.getInstance());
 
 
-        if (Build.VERSION.SDK_INT >= 24)
-        {
-            mail_resent.setText(Html.fromHtml(getString(R.string.posted_finish_resent),Html.FROM_HTML_MODE_LEGACY));
-            contact.setText(Html.fromHtml(getString(R.string.posted_finish_contact_us),Html.FROM_HTML_MODE_LEGACY));
-        }
-        else
-        {
-            mail_resent.setText(Html.fromHtml(getString(R.string.posted_finish_resent)));
-            contact.setText(Html.fromHtml(getString(R.string.posted_finish_contact_us)));
-        }
+        /**
+         * Contact us spannable text with click listener
+         */
+        String contactTextOne = "If you already request your confirmation email to be reset and you have not received that email within 30 minutes and do not see it in your Spam/Junk folder please  ";
+        String contactTextClick = "contact us ";
+        String contactTextTwo = "and include a phone number.";
 
+        Spannable word1 = new SpannableString(contactTextOne);
 
-        mail_resent.setOnClickListener(new View.OnClickListener() {
+        word1.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.colorTextDark)), 0, contactTextOne.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        contact.setText(word1);
+
+        Spannable word2 = new SpannableString(contactTextClick);
+
+        ClickableSpan myClickableSpan = new ClickableSpan() {
             @Override
-            public void onClick(View view) {
-
+            public void onClick(View widget) {
+                // There is the OnCLick. put your intent to Register class here
+                widget.invalidate();
+                Logger.printMessage("SpanHello", "click");
             }
-        });
 
-        contact.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                startActivity(new Intent(PostedFinishActivity.this,ContactUsActivity.class));
+            public void updateDrawState(TextPaint ds) {
+                ds.setColor(getResources().getColor(R.color.colorAccent));
+                ds.setUnderlineText(false);
             }
-        });
+        };
+        word2.setSpan(myClickableSpan, 0, contactTextClick.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        contact.append(word2);
+
+        Spannable word3 = new SpannableString(contactTextTwo);
+
+        word3.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.colorTextDark)), 0, contactTextTwo.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        contact.append(word3);
+
+
+        /**
+         * Mail resent spannable text with click listener
+         */
+        String mailResentPart1 = "If you do not received confirmation email within 30 minutes you can ";
+        String mailResentPart2 = "request it to be resent.";
+
+        Spannable resentWord = new SpannableString(mailResentPart1);
+
+        resentWord.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.colorTextDark)), 0, mailResentPart1.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        mail_resent.setText(resentWord);
+
+        Spannable resentWordClick = new SpannableString(mailResentPart2);
+
+        ClickableSpan resentClickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(View widget) {
+                // There is the OnCLick. put your intent to Register class here
+                widget.invalidate();
+                Logger.printMessage("SpanHello", "click");
+            }
+
+            @Override
+            public void updateDrawState(TextPaint ds) {
+                ds.setColor(getResources().getColor(R.color.colorAccent));
+                ds.setUnderlineText(false);
+            }
+        };
+        resentWordClick.setSpan(resentClickableSpan, 0, mailResentPart2.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        mail_resent.append(resentWordClick);
+
 
         findViewById(R.id.confirm_login).setOnClickListener(new View.OnClickListener() {
             @Override
