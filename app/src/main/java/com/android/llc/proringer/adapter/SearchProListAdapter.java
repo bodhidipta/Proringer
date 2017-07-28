32c5c74f7bcb8a9a2375c9d2ff71a594d422a1ac
surajcse116 <suraj.shaw@esolzmail.com>
2017-07-28 19:13:22 +0530

    28 jul

--- app/src/main/java/com/android/llc/proringer/adapter/SearchProListAdapter.java
+++ app/src/main/java/com/android/llc/proringer/adapter/SearchProListAdapter.java
@@ -1,6 +1,5 @@
 package com.android.llc.proringer.adapter;
 
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.widget.CardView;
