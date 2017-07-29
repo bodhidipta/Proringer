9b522342bfa23d4f656afa9576fb7c1ff32c462d
surajcse116 <suraj.shaw@esolzmail.com>
2017-07-29 16:19:22 +0530

    29 jul

---
+++ app/src/main/java/com/android/llc/proringer/pojo/ProjectMessage.java
@@ -0,0 +1,17 @@
+package com.android.llc.proringer.pojo;
+
+/**
+ * Created by su on 7/29/17.
+ */
+
+public class ProjectMessage {
+    boolean isopen=false;
+
+    public boolean isopen() {
+        return isopen;
+    }
+
+    public void setIsopen(boolean isopen) {
+        this.isopen = isopen;
+    }
+}
