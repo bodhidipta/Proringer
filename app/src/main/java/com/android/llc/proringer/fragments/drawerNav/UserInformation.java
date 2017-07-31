7a6f397dd83f64da34f22241c92b65d1b18b34c9
surajcse116 <suraj.shaw@esolzmail.com>
2017-07-31 10:54:57 +0530

    29 jul

--- app/src/main/java/com/android/llc/proringer/fragments/drawerNav/UserInformation.java
+++ app/src/main/java/com/android/llc/proringer/fragments/drawerNav/UserInformation.java
@@ -14,7 +14,6 @@ import android.support.v7.widget.RecyclerView;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
