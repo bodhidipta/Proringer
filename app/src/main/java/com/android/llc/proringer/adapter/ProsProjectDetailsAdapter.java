d3c535a66c3d3880a54038cc240dff68ce71f5e3
surajcse116 <suraj.shaw@esolzmail.com>
2017-08-03 12:33:42 +0530

    3 aug

--- app/src/main/java/com/android/llc/proringer/adapter/ProsProjectDetailsAdapter.java
+++ app/src/main/java/com/android/llc/proringer/adapter/ProsProjectDetailsAdapter.java
@@ -2,12 +2,15 @@ package com.android.llc.proringer.adapter;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
-import com.android.llc.proringer.R;import com.bumptech.glide.Glide;
+import com.android.llc.proringer.R;
+import com.android.llc.proringer.activities.ProjectDetailsActivity;
+import com.bumptech.glide.Glide;
 
 import org.json.JSONArray;
 
@@ -18,9 +21,20 @@ import org.json.JSONArray;
 public class ProsProjectDetailsAdapter extends RecyclerView.Adapter<ProsProjectDetailsAdapter.MyViewHolder> {
     Context context;
     JSONArray imagejsonArray;
+    int height;
+    int width;
+
     public ProsProjectDetailsAdapter(Context context,JSONArray imagejsonArray){
         this.context=context;
         this.imagejsonArray=imagejsonArray;
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        ((ProjectDetailsActivity) context).getWindowManager()
+                .getDefaultDisplay()
+                .getMetrics(displayMetrics);
+
+        height = displayMetrics.heightPixels;
+        width = displayMetrics.widthPixels;
     }
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -30,6 +44,8 @@ public class ProsProjectDetailsAdapter extends RecyclerView.Adapter<ProsProjectD
 
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
+        holder.img.getLayoutParams().width = (width-30)/5;
+        holder.img.getLayoutParams().height = (width-30)/5;
         try {
             if (!imagejsonArray.getJSONObject(position).getString("portfolio_image").equals(""))
                 Glide.with(context).load(imagejsonArray.getJSONObject(position).getString("portfolio_image")).centerCrop().into(holder.img);
