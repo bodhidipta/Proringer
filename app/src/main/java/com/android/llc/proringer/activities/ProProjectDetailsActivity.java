8d482ba723d0177aefeb97af2d1f139c90ba72ed
surajcse116 <suraj.shaw@esolzmail.com>
2017-08-03 17:51:49 +0530

    3 aug

--- app/src/main/java/com/android/llc/proringer/activities/ProProjectDetailsActivity.java
+++ app/src/main/java/com/android/llc/proringer/activities/ProProjectDetailsActivity.java
@@ -12,10 +12,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.Window;
 import android.widget.ImageView;
 import android.widget.RatingBar;
+import android.widget.ScrollView;
 
 import com.android.llc.proringer.R;
 import com.android.llc.proringer.adapter.ProDetailsServiceAreaAdapter;
@@ -29,7 +31,6 @@ import com.android.llc.proringer.helper.ProServiceApiHelper;
 import com.android.llc.proringer.utils.Logger;
 import com.android.llc.proringer.viewsmod.textview.ProRegularTextView;
 import com.bumptech.glide.Glide;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -44,14 +45,14 @@ public class ProProjectDetailsActivity extends AppCompatActivity {
     RecyclerView rcv_service, rcv_business_hour, rcv_service_area, rcv_license, rcv_project_gallery;
     JSONObject jsonObject = null;
     ProgressDialog pgDialog1,pgDialog2;
-    ProRegularTextView tv_review_btn, tv_review_value, tv_rate_value, tv_about, tv_company_name,
-            tv_user_name, tv_address, tv_city_state_zipcode, tv_business_since, tv_no_of_employee, tv_proringer_awarded, tv_business_review, tv_last_verified_on, view_all_service_area, tv_no_of_project_value, tv_no_of_picture_value, tv_business_hour;
     RatingBar rbar;
     ProsDetailsServiceAdapter proDetailsService;
     ProsDetailsBusinessHourAdapter prosDetailsBusinessHourAdapter;
     ProDetailsServiceAreaAdapter proDetailsServiceAreaAdapter;
     ProsDetailsLicenseAdapter prosDetailsLicenseAdapter;
     ProsDetailsImageAdapter prosDetailsImageAdapter;
+
+    JSONObject infoArrayJsonObject=null;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -62,26 +63,6 @@ public class ProProjectDetailsActivity extends AppCompatActivity {
         img_top = (ImageView) findViewById(R.id.img_top);
         img_profile = (ImageView) findViewById(R.id.img_profile);
         img_achievements = (ImageView) findViewById(R.id.img_achievements);
-
-        tv_review_btn = (ProRegularTextView) findViewById(R.id.tv_review_btn);
-        tv_review_value = (ProRegularTextView) findViewById(R.id.tv_review_value);
-        tv_rate_value = (ProRegularTextView) findViewById(R.id.tv_rate_value);
-        tv_about = (ProRegularTextView) findViewById(R.id.tv_about);
-        tv_company_name = (ProRegularTextView) findViewById(R.id.tv_company_name);
-        tv_user_name = (ProRegularTextView) findViewById(R.id.tv_user_name);
-        tv_address = (ProRegularTextView) findViewById(R.id.tv_address);
-        tv_city_state_zipcode = (ProRegularTextView) findViewById(R.id.tv_city_state_zipcode);
-        tv_business_hour = (ProRegularTextView) findViewById(R.id.tv_business_hour);
-
-        tv_business_since = (ProRegularTextView) findViewById(R.id.tv_business_since);
-        tv_no_of_employee = (ProRegularTextView) findViewById(R.id.tv_no_of_employee);
-        tv_proringer_awarded = (ProRegularTextView) findViewById(R.id.tv_proringer_awarded);
-        tv_business_review = (ProRegularTextView) findViewById(R.id.tv_business_review);
-        tv_last_verified_on = (ProRegularTextView) findViewById(R.id.tv_last_verified_on);
-        view_all_service_area = (ProRegularTextView) findViewById(R.id.view_all_service_area);
-        tv_no_of_project_value = (ProRegularTextView) findViewById(R.id.tv_no_of_project_value);
-        tv_no_of_picture_value = (ProRegularTextView) findViewById(R.id.tv_no_of_picture_value);
-
 
         rbar = (RatingBar) findViewById(R.id.rbar);
 
@@ -104,7 +85,7 @@ public class ProProjectDetailsActivity extends AppCompatActivity {
 
         findViewById(R.id.LLViewAll).setVisibility(View.GONE);
 
-        view_all_service_area.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.view_all_service_area).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 findViewById(R.id.LLViewAll).setVisibility(View.VISIBLE);
@@ -123,12 +104,26 @@ public class ProProjectDetailsActivity extends AppCompatActivity {
             pros_id = getIntent().getExtras().getString("pros_id");
         }
 
-        tv_review_btn.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.tv_review_btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(ProProjectDetailsActivity.this, ProReviewActivity.class);
                 intent.putExtra("pros_id", pros_id);
                 startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.tv_show_more_describetion).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                if (infoArrayJsonObject!=null && !infoArrayJsonObject.getJSONObject("about").getString("description").trim().equals("")){
+
+                        showDescribetion(infoArrayJsonObject.getJSONObject("about").getString("description"));
+                }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
         });
 
@@ -154,7 +149,7 @@ public class ProProjectDetailsActivity extends AppCompatActivity {
                                                                                                         Logger.printMessage("message", "" + message);
                                                                                                         try {
                                                                                                             jsonObject = new JSONObject(message);
-                                                                                                            JSONObject infoArrayJsonObject = jsonObject.getJSONObject("info_array");
+                                                                                                            infoArrayJsonObject = jsonObject.getJSONObject("info_array");
                                                                                                             JSONObject infoJsonObject = infoArrayJsonObject.getJSONObject("info");
 
 
@@ -165,32 +160,32 @@ public class ProProjectDetailsActivity extends AppCompatActivity {
                                                                                                             if (!infoJsonObject.getString("profile_image").equals(""))
                                                                                                                 Glide.with(ProProjectDetailsActivity.this).load(infoJsonObject.getString("profile_image")).centerCrop().into(img_profile);
 
-                                                                                                            tv_company_name.setText(infoJsonObject.getString("company_name"));
-                                                                                                            tv_user_name.setText(infoJsonObject.getString("user_name"));
-                                                                                                            tv_address.setText(infoJsonObject.getString("address"));
-                                                                                                            tv_city_state_zipcode.setText(infoJsonObject.getString("city") + ", " + infoJsonObject.getString("state") + " " + infoJsonObject.getString("zipcode"));
+                                                                                                            ((ProRegularTextView) findViewById(R.id.tv_company_name)).setText(infoJsonObject.getString("company_name"));
+                                                                                                            ((ProRegularTextView) findViewById(R.id.tv_user_name)).setText(infoJsonObject.getString("user_name"));
+                                                                                                            ((ProRegularTextView) findViewById(R.id.tv_address)).setText(infoJsonObject.getString("address"));
+                                                                                                            ((ProRegularTextView) findViewById(R.id.tv_city_state_zipcode)).setText(infoJsonObject.getString("city") + ", " + infoJsonObject.getString("state") + " " + infoJsonObject.getString("zipcode"));
 
 
-                                                                                                            tv_review_value.setText(infoArrayJsonObject.getString("total_review"));
-                                                                                                            tv_rate_value.setText(infoArrayJsonObject.getString("total_avg_review"));
+                                                                                                            ((ProRegularTextView)findViewById(R.id.tv_review_value)).setText(infoArrayJsonObject.getString("total_review"));
+                                                                                                            ((ProRegularTextView) findViewById(R.id.tv_rate_value)).setText(infoArrayJsonObject.getString("total_avg_review"));
 
 
                                                                                                             rbar.setRating(Float.parseFloat(infoArrayJsonObject.getString("total_avg_review")));
 
-                                                                                                            tv_about.setText(infoArrayJsonObject.getJSONObject("about").getString("description"));
+                                                                                                            ((ProRegularTextView) findViewById(R.id.tv_about)).setText(infoArrayJsonObject.getJSONObject("about").getString("description"));
 
                                                                                                             proDetailsService = new ProsDetailsServiceAdapter(ProProjectDetailsActivity.this, infoArrayJsonObject.getJSONArray("services"));
                                                                                                             rcv_service.setAdapter(proDetailsService);
 
                                                                                                             if (infoJsonObject.getString("business_hour").trim().equals("0")) {
                                                                                                                 rcv_business_hour.setVisibility(View.VISIBLE);
-                                                                                                                tv_business_hour.setVisibility(View.GONE);
+                                                                                                                ((ProRegularTextView) findViewById(R.id.tv_business_hour)).setVisibility(View.GONE);
                                                                                                                 prosDetailsBusinessHourAdapter = new ProsDetailsBusinessHourAdapter(ProProjectDetailsActivity.this, infoArrayJsonObject.getJSONArray("business_hours"));
                                                                                                                 rcv_business_hour.setAdapter(prosDetailsBusinessHourAdapter);
                                                                                                             } else {
                                                                                                                 rcv_business_hour.setVisibility(View.GONE);
-                                                                                                                tv_business_hour.setVisibility(View.VISIBLE);
-                                                                                                                tv_business_hour.setText("Always Open");
+                                                                                                                ((ProRegularTextView) findViewById(R.id.tv_business_hour)).setVisibility(View.VISIBLE);
+                                                                                                                ((ProRegularTextView) findViewById(R.id.tv_business_hour)).setText("Always Open");
                                                                                                             }
 
 
@@ -199,18 +194,18 @@ public class ProProjectDetailsActivity extends AppCompatActivity {
 
                                                                                                             JSONObject company_infoJsonOBJ = infoArrayJsonObject.getJSONObject("company_info");
 
-                                                                                                            tv_business_since.setText(company_infoJsonOBJ.getString("business_since"));
-                                                                                                            tv_no_of_employee.setText(company_infoJsonOBJ.getString("no_of_employee"));
-                                                                                                            tv_proringer_awarded.setText(company_infoJsonOBJ.getString("proringer_awarded"));
-                                                                                                            tv_business_review.setText(company_infoJsonOBJ.getString("business_review"));
-                                                                                                            tv_last_verified_on.setText(company_infoJsonOBJ.getString("last_verified_on"));
+                                                                                                            ((ProRegularTextView) findViewById(R.id.tv_business_since)).setText(company_infoJsonOBJ.getString("business_since"));
+                                                                                                            ((ProRegularTextView) findViewById(R.id.tv_no_of_employee)).setText(company_infoJsonOBJ.getString("no_of_employee"));
+                                                                                                            ((ProRegularTextView) findViewById(R.id.tv_proringer_awarded)).setText(company_infoJsonOBJ.getString("proringer_awarded"));
+                                                                                                            ((ProRegularTextView) findViewById(R.id.tv_business_review)).setText(company_infoJsonOBJ.getString("business_review"));
+                                                                                                            ((ProRegularTextView) findViewById(R.id.tv_last_verified_on)).setText(company_infoJsonOBJ.getString("last_verified_on"));
 
 
                                                                                                             prosDetailsLicenseAdapter = new ProsDetailsLicenseAdapter(ProProjectDetailsActivity.this, infoArrayJsonObject.getJSONArray("licence"));
                                                                                                             rcv_license.setAdapter(prosDetailsLicenseAdapter);
 
-                                                                                                            tv_no_of_project_value.setText(infoArrayJsonObject.getString("total_project"));
-                                                                                                            tv_no_of_picture_value.setText(infoArrayJsonObject.getString("total_picture"));
+                                                                                                            ((ProRegularTextView) findViewById(R.id.tv_no_of_project_value)).setText(infoArrayJsonObject.getString("total_project"));
+                                                                                                            ((ProRegularTextView) findViewById(R.id.tv_no_of_picture_value)).setText(infoArrayJsonObject.getString("total_picture"));
 
 
                                                                                                             prosDetailsImageAdapter = new ProsDetailsImageAdapter(ProProjectDetailsActivity.this, infoArrayJsonObject.getJSONArray("project_gallery"), new onOptionSelected() {
@@ -303,6 +298,32 @@ public class ProProjectDetailsActivity extends AppCompatActivity {
 
     public interface onOptionSelected {
         void onItemPassed(int position, String value);
+    }
+
+
+    private void showDescribetion(String msg){
+        final Dialog dialog = new Dialog(ProProjectDetailsActivity.this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+//                    dialog.setCancelable(false);
+        dialog.setContentView(R.layout.custom_dialogbox_pro_details_describetion);
+        //dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.WHITE));
+
+        ScrollView scrollView = (ScrollView) dialog.findViewById(R.id.scrollView);
+        ProRegularTextView tv_show_describetion = (ProRegularTextView) dialog.findViewById(R.id.tv_show_describetion);
+
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+
+        int height = displayMetrics.heightPixels;
+        int width = displayMetrics.widthPixels;
+
+        scrollView.getLayoutParams().width = (width-30);
+//        scrollView.getLayoutParams().height = (height-30)/2;
+
+        tv_show_describetion.setText(msg);
+        dialog.show();
     }
 
 }
