7b3b9dde6eb0444013cf05f3a14600ac3654f7b9
surajcse116 <suraj.shaw@esolzmail.com>
2017-08-04 12:29:12 +0530

    4 aug

--- app/src/main/java/com/android/llc/proringer/activities/ProsReviewAllListActivity.java
+++ app/src/main/java/com/android/llc/proringer/activities/ProsReviewAllListActivity.java
@@ -3,6 +3,10 @@ package com.android.llc.proringer.activities;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import com.android.llc.proringer.R;
 
 /**
@@ -11,13 +15,35 @@ import com.android.llc.proringer.R;
 
 public class ProsReviewAllListActivity extends AppCompatActivity {
     String pros_id="";
+    RecyclerView rcv_review_all;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pros_review_all_list);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         if (getIntent().getExtras() != null) {
             pros_id = getIntent().getExtras().getString("pros_id");
         }
+
+        rcv_review_all= (RecyclerView) findViewById(R.id.rcv_review_all);
+        rcv_review_all.setLayoutManager(new LinearLayoutManager(ProsReviewAllListActivity.this));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home)
+            onBackPressed();
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        setResult(GetStartedActivity.RESULT_CANCELED);
+        finish();
     }
 }
