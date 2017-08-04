43ebcfc0cfff72e5ce0bf5616e77f2eb6b84aa13
surajcse116 <suraj.shaw@esolzmail.com>
2017-08-04 11:39:20 +0530

    4 aug

---
+++ app/src/main/java/com/android/llc/proringer/activities/ProReportAbuseActivity.java
@@ -0,0 +1,39 @@
+package com.android.llc.proringer.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import com.android.llc.proringer.R;
+
+/**
+ * Created by su on 8/4/17.
+ */
+
+public class ProReportAbuseActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pro_report_abuse_review);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home)
+            onBackPressed();
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        setResult(GetStartedActivity.RESULT_CANCELED);
+        finish();
+    }
+}
